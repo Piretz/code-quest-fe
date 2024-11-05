4027b484b34507e0c407c2ad0b151bc0f564bb72
Piretz <delacruzarjay012@gmail..com>
2024-11-04 17:31:47 -0800

    app-route

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+ <h1 className='items-center text-green-600 place-items-center ' >LOGIN</h1>
+      
+    </>
+  )
+}
+
+export default page
